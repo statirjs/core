641c278024b094343166b0c761a0ebe7983e3ebd
bre30kra69cs <an.krasowsckij@gmail.com>
2020-06-13 15:17:27 +0300

    [fix] types script

--- scripts/types.js
+++ scripts/types.js
@@ -6,40 +6,91 @@ const TARGET_FILE = './index.d.ts';
 
 const FILES_WHITELIST = ['typing/internal.d.ts', 'typing/external.d.ts'];
 
-function deleteImportStatement(file) {
-  const matchs = file.match(/import.*;/) || [];
-
-  return matchs.reduce((acc, next) => acc.replace(next, ''), file);
-}
-
-function deleteExportStatement(file) {
-  const matchs = file.match(/export {};/) || [];
-
-  return matchs.reduce((acc, next) => acc.replace(next, ''), file);
-}
-
-function cleanReImport(file) {
-  const matchs = file.match(/S\./g) || [];
-
-  return matchs.reduce((acc, next) => acc.replace(next, ''), file);
-}
-
 function getFiles() {
   return FILES_WHITELIST.map((name) => {
     const file = fs.readFileSync(TYPES_DIR + '/' + name, 'utf-8');
 
-    const unimportedFile = deleteImportStatement(file);
-
-    const unexportedFile = deleteExportStatement(unimportedFile);
-
-    const cleanReImportFile = cleanReImport(unexportedFile);
-
-    return cleanReImportFile;
+    return file;
   });
 }
 
 function getBundledFile(files) {
-  return files.reduce((acc, next) => acc + next, '');
+  return files
+    .reduce((acc, next) => acc + next, '')
+    .replace(/import/g, '\nimport')
+    .replace(/export/g, '\nexport');
+}
+
+function extractImportPcakage(importLine) {
+  return importLine
+    .match(/from '.*';/g)[0]
+    .replace("from '", '')
+    .replace("';", '');
+}
+
+function extractImportValue(importLine) {
+  return importLine
+    .match(/import.*from/g)[0]
+    .replace('import ', '')
+    .replace(' from', '')
+    .replace('{ ', '')
+    .replace(' }', '')
+    .split(', ');
+}
+
+function groupByTypes(acc, next) {
+  const [key, value] = next;
+  const keys = acc.map(([existKey]) => existKey);
+
+  const isExist = keys.includes(key);
+  const nextAcc = isExist ? acc : [...acc, [key, []]];
+
+  return nextAcc.map(([existKey, existValue]) =>
+    existKey === key ? [key, [...existValue, ...value]] : [existKey, existValue]
+  );
+}
+
+function filterByTypes([key, value]) {
+  return [
+    key,
+    value.reduce((acc, next) => (acc.includes(next) ? acc : [...acc, next]), [])
+  ];
+}
+
+function formateImportString([key, value]) {
+  const isBundledImport = value.some((item) => item.includes('*'));
+
+  const valueString = value.reduce((acc, next) => `${acc}, ${next}`);
+
+  return isBundledImport ? '' : `import { ${valueString} } from "${key}";`;
+}
+
+function mergeImports(imports) {
+  const importTypes = imports.map((importLine) => [
+    extractImportPcakage(importLine),
+    extractImportValue(importLine)
+  ]);
+
+  const grouped = importTypes.reduce(groupByTypes, []);
+
+  const filtred = grouped.map(filterByTypes);
+
+  const formated = filtred.map(formateImportString);
+
+  return formated.join('\n');
+}
+
+function replaceImports(bundle) {
+  const matchs = bundle.match(/import.*;/g);
+
+  const source = matchs.reduce(
+    (acc, next) => acc.replace(next, '').replace(/S\./g, ''),
+    bundle
+  );
+
+  const imports = mergeImports(matchs);
+
+  return imports + source;
 }
 
 function writeBundle(bundle) {
@@ -55,7 +106,9 @@ function main() {
 
   const bundle = getBundledFile(files);
 
-  writeBundle(bundle);
+  const source = replaceImports(bundle);
+
+  writeBundle(source);
 }
 
 main();
